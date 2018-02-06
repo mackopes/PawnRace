c019c8af3827dc3758406c392c812cbbb9ebb54d
mp5317 <mp5317@ic.ac.uk>
2018-02-06 01:02:02 +0000

    main created for testing

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//lib includes
+#include <iostream>
+
+//project includes
+#include "board.h"
+#include "general_header.h"
+
+//using
+using std::cout;
+using std::endl;
+
+int main() {
+	Board b;
+
+	cout << b << endl;
+}
