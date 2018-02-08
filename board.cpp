68cb562ac9f7c5ec1a64b20e256993662cf1d0a1
MatkoUSKO <pialamar@gmail.com>
2018-02-08 23:43:42 +0000

    check for moves, add en_pass tile, apply move

--- board.cpp
+++ board.cpp
@@ -42,6 +42,8 @@ std::ostream& operator<< (std::ostream &out, Board const& board) {
         cout << WHITEPAWN;
       } else if (board.is_empty(i, j)) {
         cout << '.';
+      } else if (board.is_en_pass(i, j)) {
+        cout << 'x';
       } else {
         cout << ' ';
       }
@@ -78,42 +80,69 @@ int Board::number_of_pawns(tile p) const {
   return sum;
 }
 
-void Board::apply_move(Move move) {
-  //pre: move is valid (this has to be checked!)
+bool Board::apply_move(Move move) {
+  if (!is_valid_move(move)) {
+    return false;
+  }
 
   pair_ii from = move.get_from();
   pair_ii to = move.get_to();
-  if (move.is_capture() && move.is_en_passant()) {
 
+  for (int i = 0; i < BOARDSIZE; ++i) {
+    tiles_[BLACKROW + BLACKDIR][i]
+      = tiles_[BLACKROW + BLACKDIR][i] == en_pass ?
+        none
+        : tiles_[BLACKROW + BLACKDIR][i];
+    tiles_[WHITEROW + WHITEDIR][i]
+      = tiles_[WHITEROW + WHITEDIR][i] == en_pass ?
+        none
+        : tiles_[WHITEROW + WHITEDIR][i];
+  }
+  if (move.is_capture() && move.is_en_passant()) {
+    tiles_[to.first][to.second] = tiles_[from.first][to.second];
+    tiles_[from.first][from.second] = none;
+    tiles_[from.first][to.second] = none;
   } else {
     tiles_[to.first][to.second] = tiles_[from.first][to.second];
     tiles_[from.first][from.second] = none;
+    if (move.is_en_passant()) {
+      tiles_[(to.first + from.first) / 2][to.second] = en_pass;
+    }
   }
+
+  return true;
 }
 
-// bool Board::is_valid_move(Move move) {
-//   pair_ii from = move.get_from();
-//   pair_ii to = move.get_to();
-//   tile player = move.get_player;
-//   int direction = player == black ? BLACKDIR : WHITEDIR;
-//   if (!move.is_capture() && !move.is_en_passant()) { 			// normal fwd move
-//     return ((from.second == to.second)
-//             && (from.first + direction == to.first)
-//             && (tiles[from.first][from.second] == player)
-//             && (tiles[to.first][to.second] == none);
-//            );
-//   } else if (!move.is_capture() && move.is_en_passant()) { 		//double fwd move
-//     return ((from.second == to.second)
-//             && (from.first + 2 * direction == to.first)
-//             && (tiles[from.first][from.second] == player)
-//             && (tiles[to.first][to.second] == none);
-//             && (tiles[from.first + direction][from.second] == none)
-//            );
-//   } else if (move.is_capture() && !move.is_en_passant()) {		//capture, no en_pass
-//     return ((abs(from.second - to.second) == 1)
-//             && (from.first + direction == to.first)
-//             && (tiles[from.first][from.second] == player)
-//             && (tiles[to.first][to.second] == get_opposite_player(player));
-//            );
-//   }
-// }
+bool Board::is_valid_move(Move move) {
+  pair_ii from = move.get_from();
+  pair_ii to = move.get_to();
+  tile player = move.get_player();
+  int direction = player == black ? BLACKDIR : WHITEDIR;
+  if (!move.is_capture() && !move.is_en_passant()) {       // normal fwd move
+    return ((from.second == to.second)
+            && (from.first + direction == to.first)
+            && (tiles_[from.first][from.second] == player)
+            && (tiles_[to.first][to.second] == none)
+           );
+  } else if (!move.is_capture() && move.is_en_passant()) {     //double fwd move
+    return ((from.second == to.second)
+            && (from.first + 2 * direction == to.first)
+            && (tiles_[from.first][from.second] == player)
+            && (tiles_[to.first][to.second] == none)
+            && (tiles_[from.first + direction][from.second] == none)
+           );
+  } else if (move.is_capture() && !move.is_en_passant()) {   //capture, no en_pass
+    return ((abs(from.second - to.second) == 1)
+            && (from.first + direction == to.first)
+            && (tiles_[from.first][from.second] == player)
+            && (tiles_[to.first][to.second] == get_opposite_player(player))
+           );
+  } else if (move.is_capture() && move.is_en_passant()) {   //capture , en_pass
+    return ((abs(from.second - to.second) == 1)
+            && (from.first + direction == to.first)
+            && (tiles_[from.first][from.second] == player)
+            && (tiles_[to.first][to.second] == en_pass)
+            && (tiles_[from.first][to.second] == get_opposite_player(player))
+           );
+  }
+}
--- board.h
+++ board.h
@@ -15,19 +15,20 @@
 typedef std::pair<int, int> pair_ii;
 
 class Board {
-	private:
-		tile tiles_[8][8];
-		bool is_valid_move(Move move);
-	public:
-		bool is_black(int i, int j) const { return tiles_[i][j] == black; }
-		bool is_white(int i, int j) const { return tiles_[i][j] == white; }
-		bool is_empty(int i, int j) const { return tiles_[i][j] == none; }
-		Board();
-		Board(int w, int b);
-		friend std::ostream& operator<< (std::ostream &out, Board const& board);		
-		tile won() const;
-		int number_of_pawns(tile p) const;
-		void apply_move(Move move);
+ private:
+  tile tiles_[8][8];
+  bool is_valid_move(Move move);
+ public:
+  bool is_black(int i, int j) const { return tiles_[i][j] == black; }
+  bool is_white(int i, int j) const { return tiles_[i][j] == white; }
+  bool is_empty(int i, int j) const { return tiles_[i][j] == none; }
+  bool is_en_pass(int i, int j) const {return tiles_[i][j] == en_pass;}
+  Board();
+  Board(int w, int b);
+  friend std::ostream& operator<< (std::ostream &out, Board const& board);
+  tile won() const;
+  int number_of_pawns(tile p) const;
+  bool apply_move(Move move);
 };
 
 
