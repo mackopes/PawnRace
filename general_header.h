36964e219e1370942617d097f5704c76d9ebd57a
MatkoUSKO <pialamar@gmail.com>
2018-02-08 23:43:17 +0000

    add starting rows

--- general_header.h
+++ general_header.h
@@ -3,11 +3,14 @@
 
 #define BOARDSIZE 8
 
+#define BLACKROW 1
+#define WHITEROW (BOARDSIZE - 2)
+
 //these two are swapped according to ascii because of dark theme terminal usage
-#define BLACKPAWN "\u2659" 
+#define BLACKPAWN "\u2659"
 #define WHITEPAWN "\u265F"
 
 #define BLACKDIR 1
-#define WHITEDIR -1
+#define WHITEDIR (-1)
 
 #endif
