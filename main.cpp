22fe732182db9b25a4fc8743a05ee70f2ec6c544
MatkoUSKO <pialamar@gmail.com>
2018-02-10 22:02:50 +0000

    update main for testing purposes

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 //project includes
 #include "board.h"
 #include "general_header.h"
+#include "random_player.h"
+#include "game.h"
 
 //using
 using std::cout;
 using std::endl;
 
 int main() {
-  Board b(4, 7);
+  /*Board b(4, 7);
 
   cout << b << endl;
   cout << endl;
@@ -20,5 +22,10 @@ int main() {
   cout << b << endl;
 
   b.apply_move(Move(black, std::make_pair(1, 5), std::make_pair(2, 5), false, false));
-  cout << b << endl;
+  cout << b << endl;*/
+
+  Random_Player white_player = Random_Player();
+  Random_Player black_player = Random_Player();
+  Game game = Game(white_player, black_player);
+  game.start();
 }
