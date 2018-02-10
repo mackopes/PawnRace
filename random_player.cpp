361685a335dca5c4492a8fd5e8b369244aa1d696
MatkoUSKO <pialamar@gmail.com>
2018-02-10 21:21:48 +0000

    implementation of random_player, only fwd moves

---
+++ random_player.cpp
@@ -0,0 +1,57 @@
+#include "random_player.h"
+
+#define ENUMEND fwd
+#define ENUMSTART fwd
+
+Move Random_Player :: get_move(Board board) {
+  long long white, black, en_pass;
+
+  //void getll(long long & white, long long & black, long long & en_pass, Board board);
+  getll(white, black, en_pass, board);
+
+  //long long allmoves(long long black, long long white, long long en_pass, tile player, movetype mov);
+
+  std::vector <long long> move_ll;
+  move_ll.resize(ENUMEND - ENUMSTART);
+  for (int i = ENUMSTART; i <= ENUMEND; ++i) {
+    move_ll[i] = allmoves(black, white, en_pass, color(), i);
+  }
+
+  return random_element(get_all_possible_moves(move_ll));
+
+}
+
+int random_int(int min, int max) {
+  return rand() % (max - min + 1) + min;
+}
+
+template<class T>
+T random_element(std::vector<T> &elements) {
+  return elements[random_int(0, elements.size() - 1)];
+}
+}
+
+vector <Move> Random_Player :: get_all_possible_moves(vector<long long> & move_ll) {
+  int direction = color() == white ? WHITEDIR : BLACKDIR;
+  vector <Move> mv;
+  for (int i = 0; i < move_ll.size(); ++i) {
+    for (int j = 0; j < 64; ++j) {
+      long long pos = move_ll[i] & (1 << j);
+      if (pos) {
+        switch (i) {
+        case fwd:
+          long long orig_pos = color() == black ? pos << 8 : pos >> 8;
+          //Move(tile player, pair_ii from, pair_ii to, bool capture, bool en_passant);
+          mv.push_back(Move(color(), coor_to_bits(orig_pos), coor_to_bits(pos), false, false));
+          break;
+        case ffwd:
+        default:
+          break;
+        }
+      }
+    }
+  }
+
+  return mv;
+
+}
---
+++ random_player.h
@@ -0,0 +1,29 @@
+#ifndef RANDOMPLAYER_H
+#define RANDOMPLAYER_H
+
+//libs includes
+#include <vector>
+#include <random>
+
+//project includes
+#include "board.h"
+#include "tile.h"
+#include "move.h"
+#include "player.h"
+#include "random_player.h"
+
+class Random_Player : Player {
+ private:
+  //enum movetype {fwd, ffwd, capt, captpass}
+  vector <Move> get_all_possible_moves(long long fwd_moves, long long ffwd_moves,
+                                       long long capt_r_moves, long long, capt_l_moves,
+                                       long long captpass_r_moves, long long captpass_l_moves);
+ public:
+  Move get_move(Board board);
+  template<typename T>
+  T get_random_element(vector<T> & v);
+
+
+};
+
+#endif
