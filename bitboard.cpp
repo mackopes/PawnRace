576b0347b86dd65b90f950ddf2926412705adfb9
MatkoUSKO <pialamar@gmail.com>
2018-02-12 00:29:32 +0000

    bugfixes, implement ffwd, capt moves

--- bitboard.cpp
+++ bitboard.cpp
@@ -5,7 +5,25 @@
 
 //get all possible moves of attacker (direction of attacker is south, bithsift right)
 unsigned long long move_fwd_helper(unsigned long long attacker, unsigned long long deffender, unsigned long long en_pass) {
-  return ((attacker << 8L) & (~deffender));
+  return ( (attacker << 8) & (~(deffender | attacker)) );
+}
+
+unsigned long long move_ffwd_helper(unsigned long long attacker, unsigned long long deffender, unsigned long long en_pass) {
+  return ( (attacker << 16) & ( ~(deffender | attacker | (attacker << 8) | (deffender << 8)) & (0xFFUL << 24) ) );  //0xFF is mask for first row of attacker
+}
+
+unsigned long long move_capt_r_helper(unsigned long long attacker, unsigned long long deffender, unsigned long long en_pass) {
+  //0x8080808080808080 is mask for the leftmost collumn from attackers view, and he can't end here
+  return ( (attacker << 7) & (deffender & (~0x8080808080808080UL)) ); //7 is right
+}
+
+unsigned long long move_capt_l_helper(unsigned long long attacker, unsigned long long deffender, unsigned long long en_pass) {
+  //0x1010101010101010 is mask for the rightmost collumn from attackers view, and he can't end here
+  return ( (attacker << 9) & (deffender & (~0x0101010101010101UL)) ); //9 is left
+}
+
+unsigned long long move_empty_helper(unsigned long long attacker, unsigned long long deffender, unsigned long long en_pass) {
+  return 0L; //empty
 }
 
 unsigned long long rev_bites(unsigned long long side) {  //may be faster
@@ -26,8 +44,18 @@ unsigned long long allmoves(unsigned long long bl, unsigned long long wh, unsign
   case fwd:
     move_function = &move_fwd_helper;
     break;
+  case ffwd:
+    move_function = &move_ffwd_helper;
+    break;
+  case capt_r:
+    move_function = &move_capt_r_helper;
+    break;
+  case capt_l:
+    move_function = &move_capt_l_helper;
+    break;
   default:
-    move_function = &move_fwd_helper;
+    std::cerr << "unkown move in function allmoves" << std::endl;
+    move_function = &move_empty_helper;
     break;
   }
 
