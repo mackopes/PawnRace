64f31f2963970ccaa007bcf972df6a17abcfaf6e
MatkoUSKO <pialamar@gmail.com>
2018-02-12 00:31:40 +0000

    implement idea of end move

--- move.cpp
+++ move.cpp
@@ -5,10 +5,20 @@
 //project includes
 #include "move.h"
 
-Move::Move(tile player, pair_ii from, pair_ii to, bool capture, bool en_passant) {
-	player_ = player;
-	from_ = from;
-	to_= to;
-	capture_ = capture;
-	en_passant_ = en_passant;
+Move::Move(tile player, pair_ii from, pair_ii to, bool capture, bool en_passant, bool no_move) {
+  player_ = player;
+  from_ = from;
+  to_ = to;
+  capture_ = capture;
+  en_passant_ = en_passant;
+  no_move_ = no_move;
+}
+
+std::ostream& operator<< (std::ostream &out, Move const& move) {
+  out << "From: " << char (move.get_from().second + 'A') << 8 - move.get_from().first << std::endl;
+  out << "To:   " << char (move.get_to().second + 'A') << 8 - move.get_to().first;
+  if (move.is_capture()) {
+    out << " (capture)";
+  }
+  return out;
 }
--- move.h
+++ move.h
@@ -18,13 +18,17 @@ class Move {
   pair_ii to_;
   bool capture_;
   bool en_passant_;
+  bool no_move_;
  public:
-  Move(tile player, pair_ii from, pair_ii to, bool capture, bool en_passant);
+  //no move is move returned when there are no more valid moves
+  Move(tile player, pair_ii from, pair_ii to, bool capture, bool en_passant, bool no_move = false);
   bool is_capture() const { return capture_; }
   bool is_en_passant() const { return en_passant_; }
+  bool is_no_move() const { return no_move_;}
   pair_ii get_from() const { return from_; }
   pair_ii get_to() const { return to_; }
   tile get_player() const { return player_; }
+  friend std::ostream& operator<< (std::ostream &out, Move const& move);
 };
 
 
