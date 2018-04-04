180cb34340467806f4139a73f26559503a3d3b7b
MatkoUSKO <pialamar@gmail.com>
2018-04-05 00:08:59 +0200

    fix benchmark player

--- srcs/players/benchmark_player/benchmark_player.h
+++ srcs/players/benchmark_player/benchmark_player.h
@@ -7,21 +7,22 @@
 #include <sys/time.h>
 
 //project includes
+#include "../../header_files/move.h"
 #include "../player.h"
-#include "../minimax_player.h"
-#include "../random_player.h"
+#include "../minimax_player/minimax_player.h"
+#include "../random_player/random_player.h"
 
 class Benchmark_Player : public Player {
- private:
-  int turn_;
+  private:
+    int turn_;
 
-  Random_Player rp;
-  Minimax_Player mp;
- public:
-  Benchmark_Player(tile color);
-  Benchmark_Player(tile color, long max_time);
-  Move get_move(Board board);
-  ~Benchmark_Player();
+    Random_Player * rp;
+    Minimax_Player * mp;
+  public:
+    Benchmark_Player(tile color);
+    Benchmark_Player(tile color, long max_time);
+    Move get_move(Board board);
+    ~Benchmark_Player();
 };
 
 #endif
